78efcfa8d67f2dd863e4521aa9ef7432ffc4d397
Diego Feitosa <diegusmiestro@gmail.com>
2017-06-27 14:34:08 -0300

    17.06.27

--- django/app/docs/views.py
+++ django/app/docs/views.py
@@ -44,10 +44,10 @@ def docs(request):
             {'title' : 'Documentações', 'url': 'documentations/'}
         ]
     }
-    return render(request, 'documentations.html', context)
+    return render(request, 'documentations/all.html', context)
 
 @login_required
-def detail(request, id):
+def doc_detail(request, id):
     document = get_object_or_404(Document, pk=id)
     context = {
         'layout': 'materialize/index.html',
@@ -57,10 +57,10 @@ def detail(request, id):
             {'title' : document.title, 'url': 'documentations/' + str(document.id) +'/'}
         ]
     }
-    return render(request, 'detail.html', context)
+    return render(request, 'documentations/detail.html', context)
 
 @login_required
-def add(request):
+def doc_add(request):
     if request.method == "POST":
         title = request.POST.get('title')
         url = urlize(title)
@@ -89,10 +89,10 @@ def add(request):
             {'title' : 'Adicionar', 'url': 'documentations/add/'},
         ]
     }
-    return render(request, 'add.html', context)
+    return render(request, 'documentations/add.html', context)
 
 @login_required
-def edit(request, id):
+def doc_edit(request, id):
     document = get_object_or_404(Document, pk=id)
     if request.method == "POST":
         Document.objects.filter(pk=id).update(title=request.POST.get('title'), url=urlize(request.POST.get('title')), content=request.POST.get('content'))
@@ -121,10 +121,57 @@ def edit(request, id):
             {'title' : 'Edição', 'url' : 'documentations/' + str(document.id) + '/edit/'}
         ]
     }
-    return render(request, 'edit.html', context)
+    return render(request, 'documentations/edit.html', context)
 
 @login_required
-def delete(request, id):
+def doc_delete(request, id):
     documentation = Document.objects.get(pk=id)
     documentation.delete()
     return redirect('/documentations/')
+
+@login_required
+def words(request):
+    context = {
+        'layout': 'materialize/index.html',
+        'keywords': Keyword.objects.order_by('title'),
+        'breadcumb': [
+            {'title' : 'Palavras Chaves', 'url': 'keywords/'}
+        ]
+    }
+    return render(request, 'keywords/all.html', context)
+
+@login_required
+def word_detail(request, id):
+    keyword = get_object_or_404(Keyword, pk=id)
+    context = {
+        'layout': 'materialize/index.html',
+        'keyword': keyword,
+        'breadcumb': [
+            {'title' : 'Palavras-Chaves', 'url': 'keywords/'},
+            {'title' : keyword.title, 'url': 'keywords/' + str(keyword.id) +'/'}
+        ]
+    }
+    return render(request, 'keywords/detail.html', context)
+
+@login_required
+def word_edit(request, id):
+    keyword = get_object_or_404(Keyword, pk=id)
+    if request.method == "POST":
+        Keyword.objects.filter(pk=id).update(title=request.POST.get('title'), url=urlize(request.POST.get('title')))
+        redirect('/keywords/')
+    context = {
+        'layout': 'materialize/index.html',
+        'keyword': keyword,
+        'breadcumb': [
+            {'title' : 'Palavras-Chaves', 'url': 'keywords/'},
+            {'title' : keyword.title, 'url': 'keywords/' + str(keyword.id) +'/'},
+            {'title' : 'Edição', 'url' : 'keywords/' + str(keyword.id) + '/edit/'}
+        ]
+    }
+    return render(request, 'keywords/edit.html', context)
+
+@login_required
+def word_delete(request, id):
+    keyword = Keyword.objects.get(pk=id)
+    keyword.delete()
+    return redirect('/keywords/')
